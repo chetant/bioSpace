6af833a09f3be8b412a2ed06bdb6639863e25bc9
Chetan T <chetant@gmail.com>
2011-05-24 22:25:33 -0400

    Fixed minor issues in people/person views, added support for images

--- Handler/Profile.hs
+++ Handler/Profile.hs
@@ -2,6 +2,8 @@
 module Handler.Profile where
 
 import Control.Applicative((<$>))
+import Data.Text(Text)
+import qualified Data.Text as Text
 import BioSpace
 
 getPeopleR :: Handler RepHtml
@@ -13,14 +15,20 @@ getPeopleR = do
                addWidget $(widgetFile "people")
 
 -- getPersonR :: Handler RepHtml
-getPersonR profileId = do
-  person <- runDB $ get404 profileId
-  defaultLayout $ do
-    setTitle "Genspace - People"
-    addWidget $(widgetFile "person")
-
--- getPersonByNameR fName lName = do
---   (pId, person) <- runDB $ getBy404 $ ProfileFullName fName lName
+-- getPersonR profileId = do
+--   person <- runDB $ get404 profileId
+--   mu <- maybeAuth
 --   defaultLayout $ do
 --     setTitle "Genspace - People"
 --     addWidget $(widgetFile "person")
+
+mangleEmail :: Text -> Text
+mangleEmail emailAddx = user `Text.append` " < at > " `Text.append` domain
+    where (user, domain) = Text.breakOn "@" emailAddx
+
+getPersonR fName lName = do
+  (pId, person) <- runDB $ getBy404 $ ProfileFullName fName lName
+  mu <- maybeAuth
+  defaultLayout $ do
+    setTitle "Genspace - People"
+    addWidget $(widgetFile "person")
